686c0ab55c5455df2c3b114c87a6fd5396dec9a1
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2024-01-09 16:51:53 +0100

    ..

--- src/main/java/com/example/hellopizza/controller/PizzaController.java
+++ src/main/java/com/example/hellopizza/controller/PizzaController.java
@@ -1,12 +1,19 @@
 package com.example.hellopizza.controller;
 
+import com.example.hellopizza.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class PizzaController {
+    @Autowired
+    private BookRepository bookRepository;
+    @GetMapping
     public String index() {
+
         return "list";
     }
 }
---
+++ src/main/java/com/example/hellopizza/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.example.hellopizza.repository;
+
+
+import com.example.hellopizza.model.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// interfaccia che eredita da JpaRepository tutti i metodi che permettono di fare le CRUD
+// i generics chiedono: che tipo di dato è l'entità (Book) e che tipo di dato è la chiave primaria (Integer)
+public interface BookRepository extends JpaRepository<Pizza, Integer> {
+
+}
