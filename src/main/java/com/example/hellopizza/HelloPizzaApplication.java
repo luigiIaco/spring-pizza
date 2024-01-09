f5d7b2bd22f7485c39818364a9d21ffcd356f204
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2024-01-09 11:45:30 +0100

    Initial commit

---
+++ src/main/java/com/example/hellopizza/HelloPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellopizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellopizza/HelloPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellopizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
